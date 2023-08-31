ed4926c3c877001e6aca21d4dc31f6c9b9d2cb00
Huy <100940220+VoTrongHuy303@users.noreply.github.com>
2023-08-31 18:40:24 +0700

    content.js

--- scripts/content.js
+++ scripts/content.js
@@ -2,9 +2,10 @@
 console.log("Please wait ! =))");
 
 $(document).ready(function () {
- var check_empty = 1;
-function create_html(color_Text,item,funding_rate_number){
-   return txt1 = `<h4 class="${color_Text}" > <span class="circle"></span>${item.symbol} : ${(funding_rate_number * 100).toFixed(3)}%  <span class='hour'>${item.collectCycle} hour</span> </h4>`;
+
+var check_empty = 1;
+function create_html(color_Text,item,funding_rate_number,vol){
+   return txt1 = `<h4 class="${color_Text}" > <span class="circle"></span>${item.symbol} : ${(funding_rate_number * 100).toFixed(3)}%  <span class='hour'>${item.collectCycle} hour X${vol}</span> </h4>`;
 }
   $.ajax({
     url: "https://futures.mexc.com/api/v1/contract/funding_rate/",
@@ -17,27 +18,48 @@ function create_html(color_Text,item,funding_rate_number){
         funding_rate_number = parseFloat(item.fundingRate);
         if(funding_rate_number * 100 > 1.0){
           check_empty = 0;
-            var text =  create_html("positive",item,funding_rate_number)
-            $(".container_popup").append(text);
+          display(item,funding_rate_number,"positive");
+          
 
         }else if(funding_rate_number * 100 < -1.0){
           check_empty = 0;
-            var text = create_html("negative",item,funding_rate_number)
-            
-            $(".container_popup").append(text);
+          display(item,funding_rate_number,"negative");
+
         }
       });
 
       $(".proccess").remove();
       if(check_empty == 1){
-        var txt1 = `<p >Not today !</p>`;
+        var txt1 = `<p class="proccess" >Dell có con nào đâu ! =))</p>`;
         $(".container_popup").append(txt1);
       }
-      $(".container_popup").append("<h2>DONE !</h2>");
       console.warn("Done !");
     },
     error: function (error) {
       console.error("Error:", error);
     },
   });
+
+  function display(coinIfor,funding_rate_number,text_color){
+    $.ajax({
+      url: "https://futures.mexc.com/api/v1/contract/detailV2",
+      method: "GET",
+      dataType: "json",
+      success: function (response) {
+        funding_rate = response.data;
+        var volume = 0;
+        funding_rate.forEach((item, index) => {
+          if(coinIfor.symbol == item.symbol ){
+            volume = item.maxL;
+          }
+        }
+        );
+        var text =  create_html(text_color,coinIfor,funding_rate_number,volume)
+        $(".container_popup").append(text);
+      },
+      error: function (error) {
+        console.error("Error:", error);
+      },
+    });
+  }
 });
